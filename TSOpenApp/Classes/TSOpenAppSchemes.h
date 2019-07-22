a70cfe651e4b21fe6d2ec43aae08efcb1782ba17
tch85683282 <85683282@qq.com>
2019-07-22 16:05:14 +0800

    add fil

---
+++ TSOpenApp/Classes/TSOpenAppSchemes.h
@@ -0,0 +1,16 @@
+//
+//  TSOpenAppSchemes.h
+//  Pods-TSOpenApp_Example
+//
+//  Created by 唐川辉 on 2019/7/22.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TSOpenAppSchemes : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TSOpenApp/Classes/TSOpenAppSchemes.m
@@ -0,0 +1,12 @@
+//
+//  TSOpenAppSchemes.m
+//  Pods-TSOpenApp_Example
+//
+//  Created by 唐川辉 on 2019/7/22.
+//
+
+#import "TSOpenAppSchemes.h"
+
+@implementation TSOpenAppSchemes
+
+@end
