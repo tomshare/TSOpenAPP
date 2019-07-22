b112c002b1a0964b1f90d86048d9d2ad34b3a45c
tch85683282 <85683282@qq.com>
2019-07-22 15:56:47 +0800

    init pro

---
+++ Example/Pods/Target Support Files/Pods-TSOpenApp_Example/Pods-TSOpenApp_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TSOpenApp_Example : NSObject
+@end
+@implementation PodsDummy_Pods_TSOpenApp_Example
+@end
---
+++ Example/Pods/Target Support Files/TSOpenApp/TSOpenApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TSOpenApp : NSObject
+@end
+@implementation PodsDummy_TSOpenApp
+@end
